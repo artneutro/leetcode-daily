dfa56c02fe759628a0f9c366acdbd60db0146b87
Jose Lo Huang <71886421+artneutro@users.noreply.github.com>
2025-06-16 16:18:10 +0100

    2016. Maximum Difference Between Increasing Elements

---
+++ 20250616-maximum-difference-between-increasing-elements.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/maximum-difference-between-increasing-elements/
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        solution = -1
+        max_after = max(nums)
+        index = 0
+        while index < len(nums) :
+            current_min = nums[index] 
+            if index+1 < len(nums) :
+                current_max = max(nums[index+1:])
+            else :
+                break
+            if current_min >= max_after :
+                if index+1 < len(nums) :
+                    max_after = max(nums[index+1:])
+            else :
+                if max_after-current_min > solution :
+                    solution = max_after-current_min
+            index += 1
+        return solution
+        
